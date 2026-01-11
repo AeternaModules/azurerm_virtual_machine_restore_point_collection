9512a9ba454c43525a4375d46732cd1d42eb006a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:19:29 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "virtual_machine_restore_point_collections" {
+  description = "All virtual_machine_restore_point_collection resources"
+  value       = azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections
+}
+
