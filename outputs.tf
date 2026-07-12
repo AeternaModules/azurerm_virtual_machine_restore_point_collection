9b3efbfe6b6fef3b320cedd10cd31e7b75f9b807
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:21:24 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "virtual_machine_restore_point_collections" {
-  description = "All virtual_machine_restore_point_collection resources"
-  value       = azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections
-}
 output "virtual_machine_restore_point_collections_location" {
-  description = "List of location values across all virtual_machine_restore_point_collections"
-  value       = [for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : v.location]
+  description = "Map of location values across all virtual_machine_restore_point_collections, keyed the same as var.virtual_machine_restore_point_collections"
+  value       = { for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : k => v.location }
 }
 output "virtual_machine_restore_point_collections_name" {
-  description = "List of name values across all virtual_machine_restore_point_collections"
-  value       = [for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : v.name]
+  description = "Map of name values across all virtual_machine_restore_point_collections, keyed the same as var.virtual_machine_restore_point_collections"
+  value       = { for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : k => v.name }
 }
 output "virtual_machine_restore_point_collections_resource_group_name" {
-  description = "List of resource_group_name values across all virtual_machine_restore_point_collections"
-  value       = [for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : v.resource_group_name]
+  description = "Map of resource_group_name values across all virtual_machine_restore_point_collections, keyed the same as var.virtual_machine_restore_point_collections"
+  value       = { for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : k => v.resource_group_name }
 }
 output "virtual_machine_restore_point_collections_source_virtual_machine_id" {
-  description = "List of source_virtual_machine_id values across all virtual_machine_restore_point_collections"
-  value       = [for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : v.source_virtual_machine_id]
+  description = "Map of source_virtual_machine_id values across all virtual_machine_restore_point_collections, keyed the same as var.virtual_machine_restore_point_collections"
+  value       = { for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : k => v.source_virtual_machine_id }
 }
 output "virtual_machine_restore_point_collections_tags" {
-  description = "List of tags values across all virtual_machine_restore_point_collections"
-  value       = [for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : v.tags]
+  description = "Map of tags values across all virtual_machine_restore_point_collections, keyed the same as var.virtual_machine_restore_point_collections"
+  value       = { for k, v in azurerm_virtual_machine_restore_point_collection.virtual_machine_restore_point_collections : k => v.tags }
 }
 
